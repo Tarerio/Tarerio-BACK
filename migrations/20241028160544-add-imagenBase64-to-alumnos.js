fdcae81abbe7d77d4a2f4d40c946df236d76b51e
David Serrano Domínguez <105157966+daviidserranoo07@users.noreply.github.com>
2024-11-08 10:35:05 +0100

    DAVID: Arregladas migraciones

--- migrations/20241028160544-add-imagenBase64-to-alumnos.js
+++ migrations/20241028160544-add-imagenBase64-to-alumnos.js
@@ -3,11 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Alumnos', 'imagenBase64', {
-      type: Sequelize.TEXT,
-      allowNull: true,
-      unique: false
-    });
+    const tableInfo = await queryInterface.describeTable('Alumnos');
+    if (!tableInfo.imagenBase64) {
+      await queryInterface.addColumn('Alumnos', 'imagenBase64', {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        unique: false
+      });
+    }
   },
 
   async down (queryInterface, Sequelize) {
--- migrations/20241107110705-add-imagen-aula-model.js
+++ migrations/20241107110705-add-imagen-aula-model.js
@@ -3,11 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Aulas', 'imagenBase64', {
-      type: Sequelize.TEXT,
-      allowNull: true,
-      unique: false
-    });
+    const tableInfo = await queryInterface.describeTable('Aulas');
+    if (!tableInfo.imagenBase64) {
+      await queryInterface.addColumn('Aulas', 'imagenBase64', {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        unique: false
+      });
+    }
   },
 
   async down (queryInterface, Sequelize) {
